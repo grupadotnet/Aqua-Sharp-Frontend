5503373f2a3c32e34c57d7c87dbfdd443c6b7f40
ThePanToster <alcrafter@o2.pl>
2023-01-19 19:15:02 +0100

    Added more button component tests

--- src/components/button/Button.test.tsx
+++ src/components/button/Button.test.tsx
@@ -1,10 +1,50 @@
-import { render } from '@testing-library/react';
+import { createRoot } from 'react-dom/client';
+import { act } from "react-dom/test-utils";
+
 import Button from '.';
+
+const globalWithAct = global as typeof globalThis & {IS_REACT_ACT_ENVIRONMENT: boolean};
+globalWithAct.IS_REACT_ACT_ENVIRONMENT = true;
+let container: any = null;
+let root: any = null;
+beforeEach(() => {
+  act(()=>{
+    container = document.createElement("div");
+    document.body.appendChild(container);
+    root = createRoot(container);
+  });
+});
+afterEach(() => {
+  act(()=>{
+    root.unmount(container);
+    container.remove();
+    container = null;
+  });
+});
 
 describe('Button component', () =>{
     it('Children exists', () => {
-        const { getByTestId } = render(<Button type='OK' data-testid='testbutton'>Test value</Button>);
-        expect(getByTestId('testbutton').textContent).toBe('Test value');
-        expect(getByTestId('testbutton').style.backgroundColor).toBe('#1cc88a');
+        act(() => {
+            root.render(<Button type='OK'>Test value</Button>);
+        });
+        expect(container.textContent).toBe('Test value');
+    })
+    it('Children doesn\'t exist, type=\'OK\'', () => {
+        act(() => {
+            root.render(<Button type='OK'></Button>);
+        });
+        expect(container.textContent).toBe('Zatwierdź');
+    })
+    it('Children doesn\'t exist, type=\'Cancel\'', () => {
+        act(() => {
+            root.render(<Button type='Cancel'></Button>);
+        });
+        expect(container.textContent).toBe('Anuluj');
+    })
+    it('Children doesn\'t exist, type=\'Action\'', () => {
+        act(() => {
+            root.render(<Button type='Action'></Button>);
+        });
+        expect(container.textContent).toBe('');
     })
 })
