0168408dc64e4816c9150831a9afa41741afc434
wiktorrudzki <wiktor1rudzki@gmail.com>
2023-01-19 13:15:03 +0100

    i18next config

---
+++ src/assets/languages/index.ts
@@ -0,0 +1,12 @@
+export const languages = [
+  {
+    code: 'pl',
+    name: 'Polish',
+    country_code: 'pl',
+  },
+  {
+    code: 'en',
+    name: 'English',
+    country_code: 'gb',
+  },
+];
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+
 import App from './App';
+
 import './index.scss';
+import '/node_modules/flag-icons/css/flag-icons.min.css';
+
+// i18n config
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ['pl', 'en'],
+    fallbackLng: 'en',
+    detection: {
+      order: [
+        'path',
+        'cookie',
+        'navigator',
+        'htmlTag',
+        'localStorage',
+        'subdomain',
+      ],
+      caches: ['cookie'],
+      // cookieMinutes: time after cookie expires,
+      // cookieDomain: our future domain,
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+  });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  // TODO SPINNER COMES TO FALLBACK
+  <Suspense fallback={<div>loading</div>}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Suspense>
 );
