3909fd2665705ac7121a555d1e17e7678bb80474
ThePanToster <alcrafter@o2.pl>
2023-03-12 12:36:03 +0100

    Added a data tile test file

---
+++ src/components/dataTile/DataTile.test.tsx
@@ -0,0 +1,76 @@
+import { createRoot } from 'react-dom/client';
+import { act } from 'react-dom/test-utils';
+
+import DataTile from '.';
+
+const globalWithAct = global as typeof globalThis & {
+  IS_REACT_ACT_ENVIRONMENT: boolean;
+};
+globalWithAct.IS_REACT_ACT_ENVIRONMENT = true;
+let container: any = null;
+let root: any = null;
+beforeEach(() => {
+  act(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    root = createRoot(container);
+  });
+});
+afterEach(() => {
+  act(() => {
+    root.unmount(container);
+    container.remove();
+    container = null;
+  });
+});
+
+describe('DataTile component', () => {
+  // it('Children exists, no type', () => {
+  //   act(() => {
+  //     root.render(<Button>Test value</Button>);
+  //   });
+  //   expect(container.textContent).toBe('Test value');
+  // });
+  // it("Children exists, type='OK'", () => {
+  //   act(() => {
+  //     root.render(<Button type="OK">Test value</Button>);
+  //   });
+  //   expect(container.textContent).toBe('Test value');
+  // });
+  // it("Children exists, type='Cancel'", () => {
+  //   act(() => {
+  //     root.render(<Button type="Cancel">Test value</Button>);
+  //   });
+  //   expect(container.textContent).toBe('Test value');
+  // });
+  // it("Children exists, type='Action'", () => {
+  //   act(() => {
+  //     root.render(<Button type="Action">Test value</Button>);
+  //   });
+  //   expect(container.textContent).toBe('Test value');
+  // });
+  // it("Children doesn't exist, no type", () => {
+  //   act(() => {
+  //     root.render(<Button></Button>);
+  //   });
+  //   expect(container.textContent).toBe('');
+  // });
+  // it("Children doesn't exist, type='OK'", () => {
+  //   act(() => {
+  //     root.render(<Button type="OK"></Button>);
+  //   });
+  //   expect(container.textContent).toBe('Zatwierdź');
+  // });
+  // it("Children doesn't exist, type='Cancel'", () => {
+  //   act(() => {
+  //     root.render(<Button type="Cancel"></Button>);
+  //   });
+  //   expect(container.textContent).toBe('Anuluj');
+  // });
+  // it("Children doesn't exist, type='Action'", () => {
+  //   act(() => {
+  //     root.render(<Button type="Action"></Button>);
+  //   });
+  //   expect(container.textContent).toBe('');
+  // });
+});
