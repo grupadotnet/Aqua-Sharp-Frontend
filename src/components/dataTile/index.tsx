6c958fc6f8b891d291fd5efdb92c42f42b1d0dac
ThePanToster <alcrafter@o2.pl>
2023-03-16 20:57:31 +0100

    Theme is now required

--- src/components/dataTile/index.tsx
+++ src/components/dataTile/index.tsx
@@ -3,11 +3,11 @@ import './DataTile.scss';
 type Props = {
   children: string;
   title?: string;
-  theme?: 'blue' | 'green' | 'teal' | 'yellow';
+  theme: 'blue' | 'green' | 'teal' | 'yellow';
 };
 
 const DataTile = ({ children, title, theme }: Props) => {
-  const tileTheme = theme ?? 'blue';
+  const tileTheme = theme;
   return (
     <div className={`tile tile-${tileTheme}`}>
       <p>{title}</p>
