7e256bbfc3809e7450b5a9934fc4c9ae1bd8a454
ThePanToster <alcrafter@o2.pl>
2023-03-16 20:53:20 +0100

    Title doesn't have a default value now

--- src/components/dataTile/index.tsx
+++ src/components/dataTile/index.tsx
@@ -8,15 +8,9 @@ type Props = {
 
 const DataTile = ({ children, title, theme }: Props) => {
   const tileTheme = theme ?? 'blue';
-  const defaultTitle = {
-    blue: 'Aktualna temperatura',
-    green: 'Aktualne ph wody',
-    teal: 'Status światła',
-    yellow: 'Status grzałki',
-  };
   return (
-    <div className={`tile tile-${tileTheme.toLowerCase()}`}>
-      <p>{title ?? defaultTitle[tileTheme]}</p>
+    <div className={`tile tile-${tileTheme}`}>
+      <p>{title}</p>
       <p>{children}</p>
     </div>
   );
