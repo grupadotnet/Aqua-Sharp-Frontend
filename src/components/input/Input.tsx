19a179fe8e01be2b1ccc8423372c4093be699d3c
ThePanToster <alcrafter@o2.pl>
2023-04-12 20:41:14 +0200

    Rounded borders added

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,6 +1,8 @@
 import { FormControlProps } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 
-const Input = ({ ...props }: FormControlProps) => <Form.Control {...props} />;
+const Input = ({ ...props }: FormControlProps) => (
+  <Form.Control {...props} className="rounded-pill" />
+);
 
 export default Input;
