3570a2f49af2e9809c467e8570fae0f41d593bd3
Jakub Niewelt <jakubniewelt@icloud.com>
2023-04-14 20:04:38 +0200

    Added 404 handler, change dashboard path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { Working } from '@/pages/working';
 import { DashboardLayout } from '@/layout';
@@ -7,14 +7,16 @@ function App() {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route
-          path="/"
+          path="/dashboard"
           element={
             <DashboardLayout>
               <Working />
             </DashboardLayout>
           }
         />
+        <Route path="*" element={<h1 className="p-3">404 NOT FOUND</h1>} />
       </Routes>
     </div>
   );
