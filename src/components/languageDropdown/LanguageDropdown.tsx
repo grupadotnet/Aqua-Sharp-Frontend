38d47d7920474bfe314917ecdba84f627d8dd9e3
ThePanToster <alcrafter@o2.pl>
2023-04-25 15:54:14 +0200

    Dropdown translation added

--- src/components/languageDropdown/LanguageDropdown.tsx
+++ src/components/languageDropdown/LanguageDropdown.tsx
@@ -1,9 +1,11 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import { useTranslation } from 'react-i18next';
 
 const LanguageDropdown = () => {
+  const { t } = useTranslation();
   return (
-    <DropdownButton title="Wybierz język">
+    <DropdownButton title={t('select_language')}>
       <Dropdown.Item href="#/action-1">Polski</Dropdown.Item>
       <Dropdown.Item href="#/action-2">English</Dropdown.Item>
     </DropdownButton>
