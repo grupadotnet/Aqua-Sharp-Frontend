03c32caa4c6b88a7aa3721262edfc691be12219a
ThePanToster <alcrafter@o2.pl>
2023-04-27 18:56:37 +0200

    Routes grouped

--- src/App.tsx
+++ src/App.tsx
@@ -9,51 +9,25 @@ import { Working } from '@/pages/working';
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" />} />
-        <Route
-          path="/dashboard"
-          element={
-            <DashboardLayout>
-              <Working />
-            </DashboardLayout>
-          }
-        />
-        <Route
-          path="/newAquarium"
-          element={
-            <DashboardLayout>
-              <NewAquariumView />
-            </DashboardLayout>
-          }
-        />
-        <Route
-          path="/aquarium/:id"
-          element={<h1>Szczegóły danego akwarium</h1>}
-        />
-        <Route
-          path="/aquarium/:id/measurements"
-          element={<h1>Historia pomiarów akwarium</h1>}
-        />
-        <Route
-          path="/aquarium/:id/edit"
-          element={
-            <DashboardLayout>
-              <Edit />
-            </DashboardLayout>
-          }
-        />
-        <Route
-          path="/settings"
-          element={
-            <DashboardLayout>
-              <Settings />
-            </DashboardLayout>
-          }
-        />
-        <Route path="/first-run" element={<h1>Pierwsze uruchomienie</h1>} />
-        <Route path="*" element={<h1 className="p-3">404 NOT FOUND</h1>} />
-      </Routes>
+      <DashboardLayout>
+        <Routes>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Working />} />
+          <Route path="/newAquarium" element={<NewAquariumView />} />
+          <Route
+            path="/aquarium/:id"
+            element={<h1>Szczegóły danego akwarium</h1>}
+          />
+          <Route
+            path="/aquarium/:id/measurements"
+            element={<h1>Historia pomiarów akwarium</h1>}
+          />
+          <Route path="/aquarium/:id/edit" element={<Edit />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/first-run" element={<h1>Pierwsze uruchomienie</h1>} />
+          <Route path="*" element={<h1 className="p-3">404 NOT FOUND</h1>} />
+        </Routes>
+      </DashboardLayout>
     </div>
   );
 }
