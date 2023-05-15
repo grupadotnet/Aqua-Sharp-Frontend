42ea6f59e13519ae8b0b95208c44c18354f87e65
wiktorrudzki <wiktor1rudzki@gmail.com>
2023-05-15 11:22:16 +0200

    rounded button refactor

--- src/components/button/CircularButton.tsx
+++ src/components/button/CircularButton.tsx
@@ -1,14 +1,16 @@
-import { ReactElement } from 'react';
 import { Button as ButtonComponent } from 'react-bootstrap';
+
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 type Props = React.ComponentProps<typeof ButtonComponent> & {
   variant: Theme;
-  children: ReactElement;
+  icon: IconProp;
 };
 
-const CircularButton = ({ children, ...passThroughProps }: Props) => (
-  <ButtonComponent {...passThroughProps} className="rounded-circle">
-    {children}
+const CircularButton = ({ icon, variant }: Props) => (
+  <ButtonComponent variant={variant} className="btn-circle">
+    <FontAwesomeIcon icon={icon} />
   </ButtonComponent>
 );
 
