3fa649aecf0de3888443dcd7ff30035086765115
wiktorrudzki <wiktor1rudzki@gmail.com>
2023-05-18 10:47:41 +0200

    added types based on backend models

---
+++ src/types/device/device.ts
@@ -0,0 +1,21 @@
+import { Aquarium } from '../aquarium/aquarium';
+
+export type Device = {
+  deviceId: number;
+  measurementFrequency: number;
+  manualMode: boolean;
+
+  aquarium: Aquarium;
+};
+
+export type createDevice = {
+  measurementFrequency: number;
+
+  aquarium: Aquarium;
+};
+
+export type deviceConfig = {
+  aquariumId: number;
+  deviceId: number;
+  measurementFrequency: number;
+};
