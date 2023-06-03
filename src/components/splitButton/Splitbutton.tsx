77ac9f4acb98d171db74c179d105fb7fd99aa810
wiktorrudzki <wiktor1rudzki@gmail.com>
2023-06-04 01:19:55 +0200

    das

--- src/components/splitButton/Splitbutton.tsx
+++ src/components/splitButton/Splitbutton.tsx
@@ -1,13 +1,6 @@
-import React from 'react';
 import Button from 'react-bootstrap/Button';
 
-import {
-  faAngleLeft,
-  faAngleRight,
-  faDownload,
-  faGear,
-  faTrash,
-} from '@fortawesome/free-solid-svg-icons';
+import { faDownload, faGear, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './style.scss';
@@ -16,17 +9,14 @@ const Splitbutton = () => {
   return (
     <div>
       <Button variant="primary">
-        {' '}
         <FontAwesomeIcon icon={faDownload} className="icon"></FontAwesomeIcon>
         Pobierz konfigurację
       </Button>
       <Button variant="secondary">
-        {' '}
         <FontAwesomeIcon icon={faTrash} className="icon"></FontAwesomeIcon>
         Ustawienia
       </Button>
       <Button variant="danger">
-        {' '}
         <FontAwesomeIcon icon={faGear} className="icon"></FontAwesomeIcon>
         Usuń
       </Button>
