14d3e120cd64b0bd48c51a5b62ef2bfc754a3453
wiktorrudzki <wiktor1rudzki@gmail.com>
2023-06-04 01:09:42 +0200

    logging in using jwt

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -2,27 +2,49 @@ import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
+import Cookies from 'universal-cookie';
+
 import { Button, PasswordInput } from '@/components';
 import { useAuthorization } from '@/hooks';
+import { api } from '@/services/axios';
 import { Login as LoginType } from '@/types/config/login';
 
 const Login = () => {
+  const { t } = useTranslation();
   const [data, setData] = useState<LoginType>({} as LoginType);
+  const [error, setError] = useState('');
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [_, setUser] = useAuthorization();
-  const { t } = useTranslation();
   const navigate = useNavigate();
+  const cookies = new Cookies();
 
   const login = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(data.password);
-    setUser({ username: 'Wiktor', isLogged: true });
-    navigate('/');
+    api
+      .post('Config/login', {
+        password: data.password,
+      })
+      .then((res) => {
+        if (res.data && res.status === 200) {
+          const date = new Date();
+          cookies.set('Authorization', res.data, {
+            expires: new Date(date.setDate(date.getDate() + 1)),
+          });
+          setUser({ username: 'Wiktor', isLogged: true });
+          navigate('/');
+        } else {
+          setError(t('Something went wrong', { ns: 'common' }));
+        }
+      })
+      .catch(() => setError(t('Something went wrong', { ns: 'common' })));
   };
+
+  console.log(error);
 
   return (
     <form onSubmit={login}>
       <PasswordInput onChange={(e) => setData({ password: e.target.value })} />
+      {error}
       <Button variant="primary" type="submit">
         {t('log in', { ns: 'configuration' })}
       </Button>
